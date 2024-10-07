37f656ea332eae04a990cacc28ec3ecf3daf69be
Pritam <manoharipritam@gmail.com>
2024-10-07 21:04:35 +0530

    made AddProduct component dynamic

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -66,7 +66,7 @@ const Slider = () => {
                                 </button> */}
                                 <button className="inline-flex overflow-hidden text-white bg-gray-900 rounded group">
                                     <span className="px-3.5 py-3 text-white bg-main-red group-hover:bg-[#e04564] flex items-center justify-center">
-                                        <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" stroke-linejoin="round" stroke-width="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"></path></svg>
+                                        <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"></path></svg>
                                     </span>
                                     <span className="pl-4 pr-5 py-2.5">Shop Now</span>
                                 </button>
